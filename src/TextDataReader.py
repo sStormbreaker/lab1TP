38c2359a5d9179760f282f2f6734c90b2d59c072
Ivan Ivan Ivan <ivan.grom0boy@yandex.ru>
2023-01-06 22:42:07 +0300

    fix all py files for PEP8

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 from Types import DataType
 from DataReader import DataReader
+
+
 class TextDataReader(DataReader):
+
     def __init__(self) -> None:
         self.key: str = ""
         self.students: DataType = {}
+
     def read(self, path: str) -> DataType:
         with open(path, encoding='utf-8') as file:
             for line in file:
@@ -12,5 +17,6 @@ class TextDataReader(DataReader):
                     self.students[self.key] = []
                 else:
                     subj, score = line.split(":", maxsplit=1)
-                    self.students[self.key].append((subj.strip(), int(score.strip())))
+                    self.students[self.key].append(
+                        (subj.strip(), int(score.strip())))
         return self.students
