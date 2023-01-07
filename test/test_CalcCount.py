f2d69e8c0843ee25d4852110cc5d26f0fd63f10c
Ivan Ivan Ivan <ivan.grom0boy@yandex.ru>
2023-01-07 17:16:20 +0300

    add test to CalcCount

---
+++ test/test_CalcCount.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from src.Types import DataType
+from src.CalcCount import CalcCount
+import pytest
+
+
+class TestCalcCount:
+
+    @pytest.fixture()
+    def input_data(self) -> tuple[DataType, int]:
+        data: DataType = {
+            "Абрамов Петр Сергеевич":
+                [
+                    ("математика", 80),
+                    ("русский язык", 76),
+                    ("программирование", 100)
+                ],
+
+            "Петров Игорь Владимирович":
+                [
+                    ("математика", 61),
+                    ("русский язык", 80),
+                    ("программирование", 78),
+                    ("литература", 97)
+                ],
+
+            "Абрамов1 Петр Сергеевич":
+                [
+                    ("математика", 80),
+                    ("русский язык", 41),
+                    ("программирование", 100)
+                ],
+
+            "Петров1 Игорь Владимирович":
+                [
+                    ("математика", 61),
+                    ("русский язык", 80),
+                    ("программирование", 78),
+                    ("литература", 61)
+                ],
+        }
+
+        return data, 2
+
+    def test_init_calc_count(self, input_data: tuple[DataType,
+                                                      int]) -> None:
+
+        calc_rating = CalcCount(input_data[0])
+        assert input_data[0] == calc_rating.data
